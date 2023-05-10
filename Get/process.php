0f3afff52cdcdfc035e145355724e254102e4774
Lionel-prinsloo <2178389@talnet.nl>
2023-05-10 09:00:31 +0200

    Get-post opdracht

---
+++ Get/process.php
@@ -0,0 +1,13 @@
+<?php 
+$naam = $_GET["name"];
+$Achternaam = $_GET["Achternaam"];
+$leeftijd = $_GET["Leeftijd"];
+$Adress = $_GET["Adress"];
+$email = $_GET["email"];
+
+echo $naam . PHP_EOL;
+echo $Achternaam . PHP_EOL;
+echo $leeftijd . PHP_EOL;
+echo $Adress . PHP_EOL;
+echo $email . PHP_EOL;
+?>
---
+++ Post/Post.php
@@ -0,0 +1,13 @@
+<?php 
+$naam = $_POST["name"];
+$Achternaam = $_POST["Achternaam"];
+$leeftijd = $_POST["Leeftijd"];
+$Adress = $_POST["Adress"];
+$email = $_POST["email"];
+
+echo $naam . PHP_EOL;
+echo $Achternaam . PHP_EOL;
+echo $leeftijd . PHP_EOL;
+echo $Adress . PHP_EOL;
+echo $email . PHP_EOL;
+?>
